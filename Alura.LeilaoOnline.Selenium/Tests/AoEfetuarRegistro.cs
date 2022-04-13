d23a02e73845ac5ad5020c72624a4f2991687aef
Raphael Carvalho <raphilskerj@gmail.com>
2022-04-13 15:31:40 -0300

    Aula 05.Registro inválido - 03.Testando Funcionalidades do Leilão - Selenium With CSharp Part 1

--- Alura.LeilaoOnline.Selenium/Tests/AoEfetuarRegistro.cs
+++ Alura.LeilaoOnline.Selenium/Tests/AoEfetuarRegistro.cs
@@ -48,5 +48,47 @@ namespace Alura.LeilaoOnline.Selenium.Tests
             // Assert - devo ser direcionado para uma página de agradecimento
             Assert.Contains("Obrigado", driver.PageSource);
         }
+
+        [Theory]
+        [InlineData("", "koi@hotmail.com", "123", "123")]
+        [InlineData("Raphilske Koi", "koi", "123", "123")]
+        [InlineData("Raphilske Koi", "koi@hotmail.com", "123", "456")]
+        [InlineData("", "koi@hotmail.com", "123", "")]
+        public void DadoInfoInvalidasDevePermanecerNaHome(
+            string nome,
+            string email,
+            string senha,
+            string confirmaSenha)
+        {
+            // Arrange - dado chrome aberto, página inicial do sist. de leilões,
+            // E informado dados válidos para registro
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            // Nome
+            var inputNome = driver.FindElement(By.Id("Nome"));
+
+            // Email
+            var inputEmail = driver.FindElement(By.Id("Email"));
+
+            // Password
+            var inputSenha = driver.FindElement(By.Id("Password"));
+
+            // ConfirmPassword
+            var inputConfirmaSenha = driver.FindElement(By.Id("ConfirmPassword"));
+
+            // Botão de Registro
+            var botaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmaSenha.SendKeys(confirmaSenha);
+
+            // Act - efetuo o registro
+            botaoRegistro.Click();
+
+            // Assert - devo ser direcionado para uma página de agradecimento
+            Assert.Contains("section-registro", driver.PageSource);
+        }
     }
 }
